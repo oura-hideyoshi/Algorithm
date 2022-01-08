af5f3698fddaa544d21bb29228f06a420cf881f1
oura-hideyoshi <217x004x@stu.kobe-u.ac.jp>
2022-01-08 23:45:28 +0000

    python・docker導入

--- AtCoder/ABC/ABC234/D.py
+++ AtCoder/ABC/ABC234/D.py
@@ -1,17 +1,21 @@
 import io
 import sys
+import heapq
+# _INPUT = '''\
+# 11 5
+# 3 7 2 5 11 6 1 9 8 10 4
 
-_INPUT = """\
-2
-1 2 3
-aaa
-"""
-sys.stdin = io.StringIO(_INPUT)
-
-print(int(input()))
-print(list(map(int, input().split())))
-print(list(input()))
-
-2
-[1, 2, 3]
-['a', 'a', 'a']
+# '''
+# sys.stdin = io.StringIO(_INPUT)
+N, K = map(int, input().split())
+P = list(map(int, input().split()))
+que = P[0:K]
+print(min(que))
+heapq.heapify(que)
+for i in range(K, N):
+    minima = heapq.heappop(que)
+    minima = max(minima, P[i])
+    heapq.heappush(que, minima)
+    ans = heapq.heappop(que)
+    print(ans)
+    heapq.heappush(que, ans)
